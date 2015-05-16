99e4017adeef86e6662f00b6f20ecb2ec3f7098a
Nathen Drees <nadrees@gmail.com>
2015-05-15 21:24:44 -0500

    Solved "Array of N Elements"

---
+++ ArrayOfNElements/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+let f n = 
+    Seq.init n (fun _ -> n)
+    |> Seq.toArray
+
+
+let main() =
+    let input = Console.ReadLine()
+    let n = int input
+    printfn "%d" (f n).Length
+
+ 
+main()
