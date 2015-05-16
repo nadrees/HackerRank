07b250ea6b6ab14254b1eb9e16d321b8c3933682
Nathen Drees <nadrees@gmail.com>
2015-05-15 20:46:36 -0500

    Solved "Hello World N Times"

---
+++ HelloWorldNTimes/Program.fs
@@ -0,0 +1,7 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv = 
+    let n = Console.ReadLine() |> int
+    Seq.init n (fun i -> i) |> Seq.iter (fun i -> printfn "Hello World")
+    0 // return an integer exit code
