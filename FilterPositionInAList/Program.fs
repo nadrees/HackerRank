fc9730a021159b1acf3fc8275ed9d86b1b42406b
Nathen Drees <nadrees@gmail.com>
2015-05-15 21:19:28 -0500

    Solved "Filter Positions in Array"

---
+++ FilterPositionInAList/Program.fs
@@ -0,0 +1,8 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv = 
+    Seq.initInfinite (fun i -> (i, Console.ReadLine() |> int))
+    |> Seq.filter (fun (i, num) -> i % 2 <> 0)
+    |> Seq.iter (fun (i, num) -> printfn "%i" num)
+    0 // return an integer exit code
