67a38ea46ad97b6a8316d4be7dd38f80682a206e
Nathen Drees <nadrees@gmail.com>
2015-05-15 21:10:10 -0500

    Solved "Filter Array"

---
+++ FP Filter Array/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let rec FilterInputs n =
+    let next = Console.ReadLine() |> int
+    
+    match next with
+    | num when num < n -> printfn "%i" num
+    | _ -> ()
+    
+    FilterInputs n
+
+[<EntryPoint>]
+let main argv = 
+    let n = Console.ReadLine() |> int
+    FilterInputs n
+    0 // return an integer exit code
