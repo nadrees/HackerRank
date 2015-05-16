b894315b83173b5de33b7d1a4617978c323f0215
Nathen Drees <nadrees@gmail.com>
2015-05-15 21:02:56 -0500

    Solved "List Replication"

---
+++ FP List Replication/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+let rec ReadAndPrintNum n =
+    let num = Console.ReadLine() |> int
+    Seq.init n (fun _ -> n) |> Seq.iter (fun _ -> printfn "%i" num)
+    ReadAndPrintNum n
+
+[<EntryPoint>]
+let main argv = 
+    let n = Console.ReadLine() |> int
+    ReadAndPrintNum n
+    0 // return an integer exit code
